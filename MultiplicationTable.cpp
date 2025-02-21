53b6fe5e9230beb359396b589b866cd76d3ffc14
Aryan Dogra <aryandogra135@gmail.com>
2025-02-21 19:27:19 +0530

    Commit

---
+++ MultiplicationTable.cpp
@@ -0,0 +1,16 @@
+// Multiplication Table
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i * n << endl;
+    }
+}
